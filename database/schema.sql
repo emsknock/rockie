94f9db4614cb1aaac34af965d8be7c60b6bb0d4c
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-11 20:05:58 +0200

    Create db schema in sql

---
+++ database/schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE gestures (
+    id         INTEGER PRIMARY KEY,
+    shape_name TEXT    UNIQUE NOT NULL
+);
+
+INSERT INTO gestures
+       (id, shape_name)
+VALUES (0,  'rock'),
+       (1,  'paper'),
+       (2,  'scissors');
+
+--------------------------------------------------------------------------------
+
+CREATE TABLE players (
+    id        SERIAL PRIMARY KEY,
+    full_name TEXT   UNIQUE NOT NULL
+);
+
+--------------------------------------------------------------------------------
+
+CREATE TABLE unequal_matches (
+    id                SERIAL    PRIMARY KEY,
+    played_at         TIMESTAMP NOT NULL,
+    winner_player_id  INTEGER   NOT NULL REFERENCES players,
+    loser_player_id   INTEGER   NOT NULL REFERENCES players,
+    winner_gesture_id INTEGER   NOT NULL REFERENCES gestures,
+    loser_gesture_id  INTEGER   NOT NULL REFERENCES gestures,
+    INDEX (winner_player_id),
+    INDEX (loser_player_id),
+);
+
+CREATE TABLE tied_matches (
+    id          SERIAL    PRIMARY KEY,
+    played_at   TIMESTAMP NOT NULL,
+    player_a_id INTEGER   NOT NULL REFERENCES players,
+    player_b_id INTEGER   NOT NULL REFERENCES players,
+    gesture_id  INTEGER   NOT NULL REFERENCES gestures,
+    INDEX (player_a_id),
+    INDEX (player_b_id)
+);
