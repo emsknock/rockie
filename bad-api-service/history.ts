c6f0bd3548238f4a29e00e2634cb9cbebefa13df
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-12 15:01:39 +0200

    Move bad-api related helpers to own folder

--- bad-api-service/history.ts
+++ bad-api-service/history.ts
@@ -1,18 +1,30 @@
+import { GameResult } from "./types";
 const baseUrl = process.env.API_BASE_URL;
 const firstCursor = process.env.API_HISTORY_ENDPOINT;
+
 if (!firstCursor) throw Error("Please specify API_HISTORY_ENDPOINT in .env");
 if (!baseUrl) throw Error("Please specify API_BASE_URL in .env");
+
+type ApiPage = {
+    cursor: string | null;
+    data: GameResult[];
+};
 
 export async function* fetchUntilCursor(targetCursor: string | null) {
     let currentCursor: string | null = firstCursor!;
     while (currentCursor) {
         const response = await fetch(`${baseUrl}${currentCursor}`);
-        const page = await response.json();
+        const page = (await response.json()) as ApiPage;
 
         const nextCursor = page.cursor as string;
         if (nextCursor === targetCursor) return page;
 
         yield page;
         currentCursor = nextCursor;
+        await sleep(150);
     }
+}
+
+function sleep(ms: number) {
+    return new Promise((res) => setTimeout(res, ms));
 }
