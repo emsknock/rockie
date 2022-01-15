c84d0e10e447cdbad1d0f770968ecd145ae602df
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-15 18:31:50 +0200

    Use a single env value for api location

--- constants.js
+++ constants.js
@@ -2,11 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const port = Number(process.env.PORT || 3001);
-const rpsBaseUri = process.env.BAD_API_BASE_URI;
-const rpsLiveEndpoint = process.env.BAD_API_LIVE_ENDPOINT;
+const liveUri = process.env.BAD_API_LIVE_URI;
 
-if (!rpsBaseUri) throw Error("Please specify BAD_API_BASE_URI in .env");
-if (!rpsLiveEndpoint)
-    throw Error("Please specify BAD_API_LIVE_ENDPOINT in .env");
+if (!liveUri) throw Error("Please specify BAD_API_LIVE_URI in .env");
 
-module.exports = { port, rpsApiUri: `${rpsBaseUri}${rpsLiveEndpoint}` };
+module.exports = { port, liveUri };
