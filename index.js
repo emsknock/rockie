c8f292647b73b1534fc3520dbf250555c6acde09
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-15 17:57:09 +0200

    Use env files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const dotenv = require("dotenv");
 const server = require("server");
 const { json } = server.reply;
+
+dotenv.config();
 const port = Number(process.env.PORT || 3001);
 
 server({ port }, () => json({ hello: "world" }));
