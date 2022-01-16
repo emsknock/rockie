06b3c98f6c1a589ba8be3dec68b701384491a4ca
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-16 21:02:59 +0200

    Show data on front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,58 @@
-import type { NextPage } from "next";
+import type { NextPage, GetServerSideProps } from "next";
+import { getLatestMatches, MatchRecord } from "../database/queries/latest";
+import { updateDatabaseFromApi } from "../utils/update-db-from-api";
+const liveWatchUrl = process.env.LIVE_API_WATCHER_URL;
 
-const Home: NextPage = () => {
-    return <div>Hello World</div>;
+type props = {
+    ongoingGames: Array<[number, [string, string]]>;
+    latestMatches: MatchRecord[];
+};
+
+const Home: NextPage<props> = ({ ongoingGames, latestMatches }) => {
+    return (
+        <div>
+            <h1>Ongoing matches</h1>
+            <p>
+                <ul>
+                    {ongoingGames.map(([id, [aPlayerName, bPlayerName]]) => (
+                        <li key={id}>
+                            {aPlayerName} vs {bPlayerName}
+                        </li>
+                    ))}
+                </ul>
+            </p>
+            <h1>Latest matches</h1>
+            <p>
+                <ul>
+                    {latestMatches.map((match) => (
+                        <li key={match.id}>
+                            {match.winnerName} ({match.winnerHand}) vs (
+                            {match.loserHand}) {match.loserName}
+                        </li>
+                    ))}
+                </ul>
+            </p>
+        </div>
+    );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    if (!liveWatchUrl)
+        throw Error("Please specify LIVE_API_WATCHER_URL in .env");
+
+    await updateDatabaseFromApi();
+
+    const [liveGames, latestMatches] = await Promise.all([
+        fetch(liveWatchUrl).then((res) => res.json()),
+        getLatestMatches(50),
+    ]);
+
+    return {
+        props: {
+            ongoingGames: liveGames.ongoing,
+            latestMatches,
+        },
+    };
 };
 
 export default Home;
