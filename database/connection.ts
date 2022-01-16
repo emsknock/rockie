0292625f5b782b23c49aeebf03434ec2f6617ad3
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-16 21:02:34 +0200

    Parse dates as unix epocs

--- database/connection.ts
+++ database/connection.ts
@@ -2,6 +2,11 @@ import pg from "pg";
 import { Kysely, PostgresDialect } from "kysely";
 import { RpsDatabase } from "./schema";
 
+// TODO: Refactor or annotate this to be more understandable
+pg.types.setTypeParser(pg.types.builtins.TIMESTAMP, (value) =>
+    Number(new Date(`${value}+0000`))
+);
+
 const isDev = process.env.NODE_ENV === "development";
 const postgresUri = process.env.POSTGRES_URI;
 if (!postgresUri) throw new Error("Please specify POSTGRES_URI in .env");
