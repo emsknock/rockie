1bb9445e978faf4ec393eedec50d597bf335babc
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-16 20:59:13 +0200

    Create type utils for db

---
+++ database/utils.ts
@@ -0,0 +1,16 @@
+import { xxh32 } from "@node-rs/xxhash";
+
+export const normaliseMatchId = (gameId: string) => xxh32(gameId);
+
+export const gestureNameToId = (name: string) => {
+    switch (name.toLowerCase()) {
+        case "rock":
+            return 0;
+        case "paper":
+            return 1;
+        case "scissors":
+            return 2;
+        default:
+            throw Error(`Unknown gesture name: ${name}`);
+    }
+};
