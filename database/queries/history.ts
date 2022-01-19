5cbcbcd7b5f2d08ff027e0574f45b4760dc41f57
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-19 16:30:19 +0200

    Create match history query

---
+++ database/queries/history.ts
@@ -0,0 +1,73 @@
+import { GestureId } from "database/utils";
+import db from "../connection";
+
+export type MatchRecord = {
+    id: number;
+    winnerName: string | null;
+    loserName: string | null;
+    winnerHand: GestureId;
+    loserHand: GestureId;
+    playedAt: Date;
+    matchType: "tied" | "unequal";
+};
+
+export const getMatches = async (opts: {
+    before: number;
+    limit: number;
+    order: "asc" | "desc";
+}): Promise<MatchRecord[]> =>
+    db
+        .selectFrom((subquery) =>
+            subquery
+                .selectFrom("unequal_matches")
+                .select([
+                    "id",
+                    "winner_player_id",
+                    "loser_player_id",
+                    "winner_gesture_id",
+                    "loser_gesture_id",
+                    "played_at",
+                    subquery
+                        .raw<"unequal" | "tied">("'unequal'")
+                        .as("match_type"),
+                ])
+                .unionAll(
+                    subquery
+                        .selectFrom("tied_matches")
+                        .select([
+                            "id",
+                            "player_a_id as winner_player_id",
+                            "player_b_id as loser_player_id",
+                            "gesture_id as winner_gesture_id",
+                            "gesture_id as loser_gesture_id",
+                            "played_at",
+                            subquery
+                                .raw<"unequal" | "tied">("'tied'")
+                                .as("match_type"),
+                        ])
+                )
+                .as("normalised_matches")
+        )
+        .leftJoin(
+            "players as winner_player",
+            "winner_player.id",
+            "normalised_matches.winner_player_id"
+        )
+        .leftJoin(
+            "players as loser_player",
+            "loser_player.id",
+            "normalised_matches.loser_player_id"
+        )
+        .select([
+            "normalised_matches.id as id",
+            "winner_player.full_name as winnerName",
+            "loser_player.full_name as loserName",
+            "normalised_matches.winner_gesture_id as winnerHand",
+            "normalised_matches.loser_gesture_id as loserHand",
+            "played_at as playedAt",
+            "match_type as matchType",
+        ])
+        .where("played_at", "<", new Date(opts.before))
+        .orderBy("played_at", opts.order)
+        .limit(opts.limit)
+        .execute();
