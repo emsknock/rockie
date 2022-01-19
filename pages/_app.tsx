b49f6a659005e54d78dc432275984a4b42663f65
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-19 23:24:03 +0200

    Add swr config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,31 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { SWRConfig } from "swr";
 import { FC } from "react";
 
 import { LiveLayout } from "components/live-layout";
 
+const watcherUrl = process.env.LIVE_API_WATCHER_URL;
+if (!watcherUrl) throw Error("Please specify LIVE_API_WATCHER_URL in .env");
+
 const App: FC<AppProps> = ({ Component: Page, pageProps }) => (
-    <LiveLayout>
-        <Page {...pageProps} />
-    </LiveLayout>
+    <SWRConfig
+        value={{
+            fetcher: (url: string) => fetch(url).then((res) => res.json()),
+        }}
+    >
+        <Head>
+            <link
+                rel="preload"
+                href={watcherUrl}
+                as="fetch"
+                crossOrigin="anonymous"
+            />
+        </Head>
+        <LiveLayout>
+            <Page {...pageProps} />
+        </LiveLayout>
+    </SWRConfig>
 );
 
 export default App;
