47db376ec2dd446407d81a872a31956467c391e6
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-19 22:57:04 +0200

    Create socket

---
+++ bad-api-service/live/socket.ts
@@ -0,0 +1,19 @@
+const isBrowser = typeof window !== "undefined";
+const liveUrl = process.env.BAD_API_LIVE_URL;
+if (!liveUrl) throw new Error("Please specify BAD_API_LIVE_URL in .env");
+
+const sock = isBrowser && new WebSocket(liveUrl);
+
+// A copy of the bad-api-watcher's message parsing function.
+// FIXME: Git submodules would probably allow us to share the code between the
+//        nextjs app and bad-api-watcher. Unfortunately I don't have the time
+//        right now to look into it.
+export function parseApiMessage(data: any) {
+    // The API sends each event as an escaped string that needs to first be
+    // unescaped and then parsed. Easiest to just call `JSON.parse` twice.
+    const json = JSON.parse(data);
+    const event = JSON.parse(json);
+    return event;
+}
+
+export default sock;
