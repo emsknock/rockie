994dfa6d32bad26329ff158c552ff06937f93a6f
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-20 20:24:08 +0200

    Create player matches query

---
+++ database/queries/player-matches.ts
@@ -0,0 +1,81 @@
+import db from "../connection";
+import { GestureId } from "../utils";
+
+export type MatchRecord = {
+    id: number;
+    winnerName: string | null;
+    loserName: string | null;
+    winnerHand: GestureId;
+    loserHand: GestureId;
+    playedAt: number;
+    matchType: "tied" | "unequal";
+};
+
+export const getPlayerMatches = async (
+    name: string,
+    page: number,
+    limit = 50
+): Promise<MatchRecord[]> => {
+    const selectPlayerId = db
+        .selectFrom("players")
+        .select("id")
+        .where("full_name", "=", name);
+
+    return db
+        .selectFrom((subquery) =>
+            subquery
+                .selectFrom("unequal_matches")
+                .select([
+                    "id",
+                    "winner_player_id",
+                    "loser_player_id",
+                    "winner_gesture_id",
+                    "loser_gesture_id",
+                    "played_at",
+                    subquery
+                        .raw<"unequal" | "tied">("'unequal'")
+                        .as("match_type"),
+                ])
+                .unionAll(
+                    subquery
+                        .selectFrom("tied_matches")
+                        .select([
+                            "id",
+                            "player_a_id as winner_player_id",
+                            "player_b_id as loser_player_id",
+                            "gesture_id as winner_gesture_id",
+                            "gesture_id as loser_gesture_id",
+                            "played_at",
+                            subquery
+                                .raw<"unequal" | "tied">("'tied'")
+                                .as("match_type"),
+                        ])
+                )
+                .where("winner_player_id", "=", selectPlayerId)
+                .orWhere("loser_player_id", "=", selectPlayerId)
+                .orderBy("played_at", "desc")
+                .limit(limit)
+                .offset(limit * page)
+                .as("normalised_matches")
+        )
+        .leftJoin(
+            "players as winner_player",
+            "winner_player.id",
+            "normalised_matches.winner_player_id"
+        )
+        .leftJoin(
+            "players as loser_player",
+            "loser_player.id",
+            "normalised_matches.loser_player_id"
+        )
+        .select([
+            "normalised_matches.id as id",
+            "winner_player.full_name as winnerName",
+            "loser_player.full_name as loserName",
+            "normalised_matches.winner_gesture_id as winnerHand",
+            "normalised_matches.loser_gesture_id as loserHand",
+            "played_at as playedAt",
+            "match_type as matchType",
+        ])
+        .execute();
+};
