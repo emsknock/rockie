286843ccf3b3f67590e997d22ca731ff4f37bb59
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-20 17:38:44 +0200

    Don't throw for missing private variables in browser

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,11 +1,14 @@
+const isBrowser = typeof window !== "undefined";
+const isNode = !isBrowser;
 const missing = ([varName]: TemplateStringsArray) =>
     `Please specify ${varName} in .env`;
 
 export const postgresUrl = process.env.POSTGRES_URL!;
-if (!postgresUrl) throw Error(missing`POSTGRES_URL`);
+if (isNode && !postgresUrl) throw Error(missing`POSTGRES_URL`);
 export const historyUrl = process.env.BAD_API_HISTORY_URL!;
-if (!historyUrl) throw Error(missing`BAD_API_HISTORY_URL`);
+if (isNode && !historyUrl) throw Error(missing`BAD_API_HISTORY_URL`);
 export const liveUrl = process.env.NEXT_PUBLIC_BAD_API_LIVE_URL!;
-if (!liveUrl) throw Error(missing`NEXT_PUBLIC_BAD_API_LIVE_URL`);
-export const watcherUrl = process.env.LIVE_API_WATCHER_URL!;
-if (!watcherUrl) throw Error(missing`LIVE_API_WATCHER_URL`);
+if (isBrowser && !liveUrl) throw Error(missing`NEXT_PUBLIC_BAD_API_LIVE_URL`);
+export const watcherUrl = process.env.NEXT_PUBLIC_LIVE_API_WATCHER_URL!;
+if (isBrowser && !watcherUrl)
+    throw Error(missing`NEXT_PUBLIC_LIVE_API_WATCHER_URL`);
