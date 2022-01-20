881ec8a27560b1ae1b55ea1452a93823c4bfa2f5
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-20 18:48:23 +0200

    Display live matches

--- components/live-layout.tsx
+++ components/live-layout.tsx
@@ -1,9 +1,28 @@
 import { FC } from "react";
+import useLiveState from "bad-api-service/live/state";
 
 export const LiveLayout: FC = ({ children }) => {
+    const matches = useLiveState((s) => s.matches);
     return (
         <>
-            <div>Live layout!</div>
+            <h1>Live layout!</h1>
+            <h2>Ongoing games</h2>
+            <ul>
+                {matches.map((match) => (
+                    <li key={match.gameId}>
+                        {match.isResolved ? (
+                            <>
+                                {match.aPlayer} ({match.aPlayerGesture}) vs (
+                                {match.bPlayerGesture}) {match.bPlayer}
+                            </>
+                        ) : (
+                            <>
+                                {match.aPlayer} vs {match.bPlayer}
+                            </>
+                        )}
+                    </li>
+                ))}
+            </ul>
             {children}
         </>
     );
