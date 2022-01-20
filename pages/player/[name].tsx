b283158f575789950babc7c35596c0a929699012
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-20 20:24:54 +0200

    Show actual data from db

--- pages/player/[name].tsx
+++ pages/player/[name].tsx
@@ -1,16 +1,29 @@
+import type { MatchRecord } from "database/queries/player-matches";
 import type { StatsRecord } from "database/queries/player-stats";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 
+const apiStatsPath = (name: string) => `/api/player-stats?name=${name}`;
+const apiMatchesPath = (name: string, page: number) =>
+    `/api/player-matches?name=${name}&page=${page}`;
+
 export default function Player() {
     const { query } = useRouter();
     const playerName = query.name as string;
-    const stats = useSWR<StatsRecord>(`/api/player-stats?name=${playerName}`);
+    const page = Number((query.page as string) ?? 0);
+
+    const stats = useSWR<StatsRecord>(apiStatsPath(playerName));
+    const matches = useSWR<MatchRecord[]>(apiMatchesPath(playerName, 0));
 
     return (
         <>
             <h1>{playerName}</h1>
-            {stats.data?.wonMatches.count}
+            <p>{stats.data?.wonMatches.count}</p>
+            <ul>
+                {matches.data?.map((match) => (
+                    <li key={match.id}>{match.id}</li>
+                ))}
+            </ul>
         </>
     );
 }
