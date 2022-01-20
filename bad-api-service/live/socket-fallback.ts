7a2a9f96eb4d473bd1af05db305daeb1ca637afd
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-20 23:04:54 +0200

    Unused file

--- bad-api-service/live/socket-fallback.ts
+++
@@ -1,6 +0,0 @@
-import { watcherUrl } from "utils/env";
-
-export default async function fallbackFetch() {
-    const state = await fetch(watcherUrl).then((res) => res.json());
-    return state;
-}
