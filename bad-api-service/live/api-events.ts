2b7f07589700a8317481964b5f488826d499fd34
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-21 00:08:04 +0200

    Move event parser to its own file

---
+++ bad-api-service/live/api-events.ts
@@ -0,0 +1,56 @@
+import { GestureId } from "utils/gestures";
+import { normaliseGameId } from "utils/hash-id";
+
+export async function parseApiMessage(data: any) {
+    // FIXME: Git submodules would probably allow us to share the code between the
+    //        nextjs app and bad-api-watcher. Unfortunately I don't have the time
+    //        right now to look into it.
+
+    type GameBegin = {
+        type: "GAME_BEGIN";
+        gameId: string;
+        playerA: { name: string };
+        playerB: { name: string };
+    };
+    type GameResult = {
+        type: "GAME_RESULT";
+        gameId: string;
+        t: number;
+        playerA: { name: string; played: "ROCK" | "PAPER" | "SCISSORS" };
+        playerB: { name: string; played: "ROCK" | "PAPER" | "SCISSORS" };
+    };
+
+    // The API sends each event as an escaped string that needs to first be
+    // unescaped and then parsed. Easiest to just call `JSON.parse` twice.
+    const json = JSON.parse(data);
+    const event = JSON.parse(json) as GameBegin | GameResult;
+
+    return event.type === "GAME_BEGIN"
+        ? {
+              type: "GAME_BEGIN",
+              id: await normaliseGameId(event.gameId),
+              aPlayer: event.playerA.name,
+              bPlayer: event.playerB.name,
+          }
+        : {
+              type: "GAME_RESULT",
+              id: await normaliseGameId(event.gameId),
+              t: event.t,
+              aPlayer: event.playerA.name,
+              bPlayer: event.playerB.name,
+              aGesture:
+                  GestureId[
+                      event.playerA.played.toLowerCase() as
+                          | "rock"
+                          | "paper"
+                          | "scissors"
+                  ],
+              bGesture:
+                  GestureId[
+                      event.playerB.played.toLowerCase() as
+                          | "rock"
+                          | "paper"
+                          | "scissors"
+                  ],
+          };
+}
