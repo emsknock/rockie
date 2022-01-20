e4180b9499662dff984d83c28acd8db8ab5ed23e
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-21 01:54:28 +0200

    Show an update button when there's new data available

--- pages/player/[name].tsx
+++ pages/player/[name].tsx
@@ -1,7 +1,9 @@
 import type { MatchRecord } from "database/queries/player-matches";
 import type { StatsRecord } from "database/queries/player-stats";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import useLiveState from "bad-api-service/live/state";
 
 const apiStatsPath = (name: string) => `/api/player-stats?name=${name}`;
 const apiMatchesPath = (name: string, page: number) =>
@@ -10,17 +12,50 @@ const apiMatchesPath = (name: string, page: number) =>
 export default function Player() {
     const { query } = useRouter();
     const playerName = query.name as string;
-    const page = Number((query.page as string) ?? 0);
+    const pageNumber = Number((query.page as string) ?? 0);
 
-    const stats = useSWR<StatsRecord>(apiStatsPath(playerName));
-    const matches = useSWR<MatchRecord[]>(apiMatchesPath(playerName, page));
+    const playerStats = useSWR<StatsRecord>(apiStatsPath(playerName));
+    const playerHistory = useSWR<MatchRecord[]>(
+        apiMatchesPath(playerName, pageNumber)
+    );
+
+    const liveMatches = useLiveState((s) => s.matches);
+    const [isNewDataAvailbale, setNewDataAvailable] = useState(false);
+    useEffect(() => {
+        // Is there a resolved match where
+        // * this player was a participant and
+        // * it isn't included on this page already
+        const newResolvedMatchExists = liveMatches.some(
+            (match) =>
+                match.isResolved &&
+                (match.aPlayer === playerName ||
+                    match.bPlayer === playerName) &&
+                !playerHistory.data?.map((g) => g.id).includes(match.id)
+        );
+        if (newResolvedMatchExists) setNewDataAvailable(true);
+    }, [liveMatches.map((game) => game.id)]);
+
+    useEffect(() => {
+        if (isNewDataAvailbale) playerStats.mutate();
+    }, [isNewDataAvailbale]);
+
+    useEffect(() => {
+        setNewDataAvailable(false);
+    }, [playerName]);
 
     return (
         <>
             <h1>{playerName}</h1>
-            <p>{stats.data?.wonMatches.count}</p>
+            <p>{playerStats.data?.wonMatches.count}</p>
+            {isNewDataAvailbale && (
+                <p>
+                    <button onClick={() => playerHistory.mutate()}>
+                        New data available — click to update
+                    </button>
+                </p>
+            )}
             <ul>
-                {matches.data?.map((match) => (
+                {playerHistory.data?.map((match) => (
                     <li key={match.id}>{match.id}</li>
                 ))}
             </ul>
