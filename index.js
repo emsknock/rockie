145105b47c7c95f912cedb29edaa78bd06897759
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-21 19:09:09 +0200

    Disallow caching

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const WebSocket = require("ws");
 const server = require("server");
 const cors = require("cors");
 const store = require("./game-store");
-const { json } = server.reply;
+const { json, header } = server.reply;
 
 const ws = new WebSocket(liveUrl);
 ws.on("message", store.handleEvent);
 
-server({ port }, [server.utils.modern(cors()), () => json(store.readout())]);
+server({ port }, [
+    server.utils.modern(cors()),
+    // Safari doesn't respect "no-store", but does respect "Vary: *"
+    () => header("Cache-Control", "no-store"),
+    () => header("Vary", "*"),
+    () => json(store.readout()),
+]);
