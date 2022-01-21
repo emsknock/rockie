3c467c30488995169f6fbbea0b9d134898a6dfa6
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-21 19:54:09 +0200

    Return null instead of false on node for socket

--- bad-api-service/live/socket.ts
+++ bad-api-service/live/socket.ts
@@ -3,6 +3,6 @@ import { liveUrl } from "utils/env";
 const isBrowser = typeof window !== "undefined";
 
 // Socket related code doesn't need to run server side
-const sock = isBrowser && new WebSocket(liveUrl);
+const sock = isBrowser ? new WebSocket(liveUrl) : null;
 
 export default sock;
