c1c4dd091e0a642fa3df4a57b3bc792a469c2552
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-21 20:46:34 +0200

    Rewrite database updater

--- utils/update-db-from-api.ts
+++ utils/update-db-from-api.ts
@@ -1,29 +1,52 @@
-import { fetchUntilCursor } from "../bad-api-service/history";
-import db from "../database/connection";
-import { gestureId } from "../database/utils";
-import { xxh32 } from "@node-rs/xxhash";
-
-const normaliseMatchId = (gameId: string) => xxh32(gameId);
+import { fetchUntilCursor } from "bad-api-service/history";
+import db from "database/connection";
+import { GestureId } from "utils/gestures";
+import { gameIdNormaliser } from "utils/hash-id";
 
 export const updateDatabaseFromApi = async () => {
-    // TODO: Store the latest saved cursor somewhere and only traverse that far
-    for await (const page of fetchUntilCursor(
-        "/rps/history?cursor=KDpAtv7_Hd36"
-    )) {
-        await db
-            .insertInto("staging_matches")
-            .values(
-                page.data.map((match) => ({
-                    id: normaliseMatchId(match.gameId),
-                    played_at: match.t,
-                    player_a_full_name: match.playerA.name,
-                    player_b_full_name: match.playerB.name,
-                    player_a_gesture_id: gestureId(match.playerA.played),
-                    player_b_gesture_id: gestureId(match.playerB.played),
-                }))
-            )
-            .onConflict((oc) => oc.doNothing())
-            .execute();
-        await db.raw("truncate table staging_matches").execute();
-    }
+    const normalise = await gameIdNormaliser;
+
+    db.transaction().execute(async (trx) => {
+        const { lastCursor } = await trx
+            .selectFrom("app_meta")
+            .select("last_cursor as lastCursor")
+            .executeTakeFirstOrThrow();
+
+        let newLastCursor: string | null = null;
+
+        for await (const page of fetchUntilCursor(lastCursor)) {
+            if (newLastCursor === null) newLastCursor = page.nextCursor;
+
+            await trx
+                .insertInto("staging_matches")
+                .values(
+                    page.data.map((game) => ({
+                        id: normalise(game.gameId),
+                        played_at: game.t,
+                        player_a_full_name: game.playerA.name,
+                        player_b_full_name: game.playerB.name,
+                        player_a_gesture_id:
+                            GestureId[
+                                game.playerA.played.toLowerCase() as
+                                    | "rock"
+                                    | "paper"
+                                    | "scissors"
+                            ],
+                        player_b_gesture_id:
+                            GestureId[
+                                game.playerB.played.toLowerCase() as
+                                    | "rock"
+                                    | "paper"
+                                    | "scissors"
+                            ],
+                    }))
+                )
+                .onConflict((oc) => oc.doNothing())
+                .execute();
+
+            await trx.raw("truncate table staging_matches").execute();
+        }
+
+        await trx.updateTable("app_meta").set({ last_cursor: newLastCursor });
+    });
 };
