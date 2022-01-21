ad4246c46b567217761b65859f93c84ad217f8e7
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-21 23:03:35 +0200

    Add env variables

--- constants.js
+++ constants.js
@@ -2,7 +2,11 @@ require("dotenv-flow").config();
 
 const port = Number(process.env.PORT || 3001);
 const liveUrl = process.env.BAD_API_LIVE_URL;
+const historyUrl = process.env.BAD_API_HISTORY_URL;
+const postgresUrl = process.env.POSTGRES_URL;
 
 if (!liveUrl) throw Error("Please specify BAD_API_LIVE_URL in .env");
+if (!historyUrl) throw Error("Please specify BAD_API_HISTORY_URL in .env");
+if (!postgresUrl) throw Error("Please specify POSTGRES_URL in .env");
 
-module.exports = { port, liveUrl };
+module.exports = { port, liveUrl, historyUrl, postgresUrl };
