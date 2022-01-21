52d8168e0a29c9919d7a8d88f3ea9e9aaa86bf5a
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-21 20:15:35 +0200

    Create cursor getter and setter queries

---
+++ database/queries/bad-api-cursors.ts
@@ -0,0 +1,12 @@
+import db from "database/connection";
+
+export const getLastCursor = async (): Promise<string> => {
+    const result = await db
+        .selectFrom("app_meta")
+        .select("last_cursor")
+        .executeTakeFirst()!;
+    return result!.last_cursor;
+};
+
+export const setLastCursor = (cursor: string) =>
+    db.updateTable("app_meta").set({ last_cursor: cursor }).execute();
