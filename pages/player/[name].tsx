8159d70d1dfe52f4107893193286ac3483a6064b
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-21 16:09:19 +0200

    Shorter variable names

--- pages/player/[name].tsx
+++ pages/player/[name].tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import useSWR from "swr";
 import useLiveState from "bad-api-service/live/state";
+import { GetServerSideProps } from "next";
 
 const apiStatsPath = (name: string) => `/api/player-stats?name=${name}`;
 const apiMatchesPath = (name: string, page: number) =>
@@ -12,13 +13,11 @@ const apiMatchesPath = (name: string, page: number) =>
 
 export default function Player() {
     const { query } = useRouter();
-    const playerName = query.name as string;
-    const pageNumber = Number((query.page as string) ?? 0);
+    const name = query.name as string;
+    const page = Number((query.page as string) ?? 0);
 
-    const playerStats = useSWR<StatsRecord>(apiStatsPath(playerName));
-    const playerHistory = useSWR<MatchRecord[]>(
-        apiMatchesPath(playerName, pageNumber)
-    );
+    const playerStats = useSWR<StatsRecord>(apiStatsPath(name));
+    const playerHistory = useSWR<MatchRecord[]>(apiMatchesPath(name, page));
 
     const liveMatches = useLiveState((s) => s.matches);
     const [isNewDataAvailbale, setNewDataAvailable] = useState(false);
@@ -29,23 +28,22 @@ export default function Player() {
         const newResolvedMatchExists = liveMatches.some(
             (match) =>
                 match.isResolved &&
-                (match.aPlayer === playerName ||
-                    match.bPlayer === playerName) &&
+                (match.aPlayer === name || match.bPlayer === name) &&
                 !playerHistory.data?.map((g) => g.id).includes(match.id)
         );
         if (newResolvedMatchExists) setNewDataAvailable(true);
-    }, [liveMatches.map((game) => game.id), playerName, pageNumber]);
+    }, [liveMatches.map((game) => game.id), name, page]);
 
     useEffect(
         () => void (isNewDataAvailbale && playerStats.mutate()),
         [isNewDataAvailbale]
     );
 
-    useEffect(() => void setNewDataAvailable(false), [playerName]);
+    useEffect(() => void setNewDataAvailable(false), [name]);
 
     return (
         <>
-            <h1>{playerName}</h1>
+            <h1>{name}</h1>
             <p>{playerStats.data?.wonMatches.count}</p>
             {isNewDataAvailbale && (
                 <p>
@@ -55,12 +53,12 @@ export default function Player() {
                 </p>
             )}
             <nav>
-                <Link href={`/player/${playerName}?page=${pageNumber - 1}`}>
-                    <a>Prev ({pageNumber - 1})</a>
+                <Link href={`/player/${name}?page=${page - 1}`}>
+                    <a>Prev ({page - 1})</a>
                 </Link>{" "}
-                Page {pageNumber}{" "}
-                <Link href={`/player/${playerName}?page=${pageNumber + 1}`}>
-                    <a>Next {pageNumber + 1}</a>
+                Page {page}{" "}
+                <Link href={`/player/${name}?page=${page + 1}`}>
+                    <a>Next {page + 1}</a>
                 </Link>
             </nav>
             <ul>
