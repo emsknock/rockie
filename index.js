827fc4871c0903f734ba514100e8096e6de4dc5f
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-21 23:07:28 +0200

    Reorder imports

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const { port, liveUrl } = require("./utils/env");
+
 const WebSocket = require("ws");
 const server = require("server");
 const cors = require("cors");
 const store = require("./live/game-store");
-const { json, header } = server.reply;
 
 const ws = new WebSocket(liveUrl);
 ws.on("message", store.handleEvent);
 
+const { json, header } = server.reply;
 server({ port }, [
     server.utils.modern(cors()),
     // Safari doesn't respect "no-store", but does respect "Vary: *"
