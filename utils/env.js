d9156400982e210149a2304709453e61cacf6c2b
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 00:08:16 +0200

    Postgres → database

--- utils/env.js
+++ utils/env.js
@@ -3,11 +3,11 @@ require("dotenv-flow").config();
 const port = Number(process.env.PORT || 3001);
 const liveUrl = process.env.BAD_API_LIVE_URL;
 const historyUrl = process.env.BAD_API_HISTORY_URL;
-const postgresUrl = process.env.POSTGRES_URL;
+const postgresUrl = process.env.DATABASE_URL;
 const dbCronExpr = process.env.DB_CRON_EXPR ?? "*/1 * * * *";
 
 if (!liveUrl) throw Error("Please specify BAD_API_LIVE_URL in .env");
 if (!historyUrl) throw Error("Please specify BAD_API_HISTORY_URL in .env");
-if (!postgresUrl) throw Error("Please specify POSTGRES_URL in .env");
+if (!postgresUrl) throw Error("Please specify DATABASE_URL in .env");
 
 module.exports = { port, liveUrl, historyUrl, postgresUrl, dbCronExpr };
