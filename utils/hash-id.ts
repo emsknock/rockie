7bb7a2ec1b2752d584eaabe34acc4996b899bfff
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-21 23:22:28 +0200

    Rewrite game id normaliser

--- utils/hash-id.ts
+++ utils/hash-id.ts
@@ -1,8 +1,5 @@
 import xxhash from "xxhash-wasm";
 
-export const gameIdNormaliser = new Promise<(gameId: string) => number>(
-    async () => {
-        const { h32 } = await xxhash();
-        return (gameId: string) => h32(gameId) - 0x80000000;
-    }
-);
+export const gameIdNormaliser = xxhash()
+    .then((hasher) => hasher.h32)
+    .then((hash) => (gameId: string) => hash(gameId) - 0x80000000);
