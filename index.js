1e9deedfe4d00bd21f9ee12cde5ccf6e57bb266d
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-21 16:41:52 +0200

    Add CORS middleware

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { port, liveUrl } = require("./constants");
 const WebSocket = require("ws");
 const server = require("server");
+const cors = require("cors");
 const store = require("./game-store");
 const { json } = server.reply;
 
 const ws = new WebSocket(liveUrl);
-
 ws.on("message", store.handleEvent);
 
-server({ port }, () => json(store.readout()));
+server({ port }, [server.utils.modern(cors()), () => json(store.readout())]);
