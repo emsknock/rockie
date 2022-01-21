317b97a67078b97e12f7852cf5e4448ee487417b
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 00:42:05 +0200

    Rewrite

--- bad-api-service/history/database/queries/bad-api-cursors.ts
+++ bad-api-service/history/database/queries/bad-api-cursors.ts
@@ -1,12 +1,9 @@
 import db from "bad-api-service/history/database/connection";
 
-export const getLastCursor = async (): Promise<string> => {
-    const result = await db
+export const getLastCursor = async (): Promise<string | null> => {
+    const { lastCursor } = await db
         .selectFrom("app_meta")
-        .select("last_cursor")
-        .executeTakeFirst()!;
-    return result!.last_cursor;
+        .select("last_cursor as lastCursor")
+        .executeTakeFirstOrThrow();
+    return lastCursor;
 };
-
-export const setLastCursor = (cursor: string) =>
-    db.updateTable("app_meta").set({ last_cursor: cursor }).execute();
