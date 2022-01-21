8ba4e42634a5cc205be4dea4118bea46f19a4111
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 01:56:02 +0200

    Move database queries etc to history module exports

--- bad-api-service/history/database/index.ts
+++
@@ -1,8 +0,0 @@
-export { default } from "./connection";
-
-export { default as getLastCursor } from "./queries/bad-api-cursors";
-export { default as getPlayerMatches } from "./queries/player-matches";
-export { default as getPlayerStats } from "./queries/player-stats";
-
-export type { MatchRecord } from "./queries/player-matches";
-export type { StatsRecord } from "./queries/player-stats";
--- bad-api-service/history/index.ts
+++ bad-api-service/history/index.ts
@@ -1,1 +1,10 @@
+export { default } from "./database/connection";
+
 export { default as refreshDatabase } from "./update-db-from-api";
+
+export { default as getLastCursor } from "./database/queries/bad-api-cursors";
+export { default as getPlayerMatches } from "./database/queries/player-matches";
+export { default as getPlayerStats } from "./database/queries/player-stats";
+
+export type { MatchRecord } from "./database/queries/player-matches";
+export type { StatsRecord } from "./database/queries/player-stats";
