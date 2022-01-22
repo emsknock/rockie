248f257ca0c5bdaa1bcb1e48f3539146bb5b426d
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 07:12:04 +0200

    Allow both cursor and page navigation

--- pages/api/[player]/history.ts
+++ pages/api/[player]/history.ts
@@ -1,6 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { refreshDatabase, getPlayerMatches } from "bad-api-service/history";
 import { isValidCursor } from "utils/history-cursors";
+import {
+    refreshDatabase,
+    getHistoryByCursor,
+    getHistoryByPage,
+} from "bad-api-service/history";
 
 export default async function handler(
     req: NextApiRequest,
@@ -9,12 +13,16 @@ export default async function handler(
     await refreshDatabase();
 
     const name = req.query.player as string;
+    const page = Number(req.query.page ?? 0);
     const cursor = req.query.cursor as string | undefined;
 
+    if (isNaN(page)) return res.status(400).send("Invalid page");
     if (cursor && !isValidCursor(cursor))
         return res.status(400).send("Invalid cursor");
 
-    const page = await getPlayerMatches(name, cursor);
-
-    return res.json(page);
+    return res.json(
+        cursor
+            ? await getHistoryByCursor(name, cursor)
+            : await getHistoryByPage(name, page)
+    );
 }
