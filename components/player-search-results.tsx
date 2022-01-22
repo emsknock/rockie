61ef20d9f59e6608dae6d9ef0f277651dac964a3
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 17:16:02 +0200

    Move results' container to same file

--- components/player-search-results.tsx
+++ components/player-search-results.tsx
@@ -6,7 +6,7 @@ import clsx from "clsx";
 export const PlayerSearchResults: FC<{ prefix: string }> = ({ prefix }) => {
     const { data } = useSWR<string[]>(`/api/find-names?name=${prefix}`);
     return (
-        <>
+        <ol className={clsx("p-2", "flex flex-wrap gap-2 justify-center")}>
             {data?.map((name) => (
                 <li key={name} className="m-2">
                     <Link href={`/player/${name}`}>
@@ -21,6 +21,6 @@ export const PlayerSearchResults: FC<{ prefix: string }> = ({ prefix }) => {
                     </Link>
                 </li>
             ))}
-        </>
+        </ol>
     );
 };
