c574ce8049230866c78ed06e0f5a231792cf5091
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 04:57:47 +0200

    Add exports

--- bad-api-service/history/index.ts
+++ bad-api-service/history/index.ts
@@ -2,9 +2,15 @@ export { default } from "./database/connection";
 
 export { default as refreshDatabase } from "./update-db-from-api";
 
+export {
+    default as getPlayerMatches,
+    isValidCursor,
+} from "./database/queries/player-matches";
 export { default as getLastCursor } from "./database/queries/bad-api-cursors";
-export { default as getPlayerMatches } from "./database/queries/player-matches";
 export { default as getPlayerStats } from "./database/queries/player-stats";
 
-export type { MatchRecord } from "./database/queries/player-matches";
+export type {
+    MatchRecord,
+    PlayerMatchesPage,
+} from "./database/queries/player-matches";
 export type { StatsRecord } from "./database/queries/player-stats";
