e35c550e8800416343acdf0d3e1a5b827370cc41
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 14:16:04 +0200

    Allow flipping

--- components/gesture.tsx
+++ components/gesture.tsx
@@ -11,16 +11,32 @@ import {
 export function Gesture({
     id,
     fill = true,
+    flip = false,
 }: {
     id: GestureId;
     fill?: boolean;
+    flip?: boolean;
 }) {
+    const props = flip ? { transform: "scale(-1, 1)" } : {};
+
     switch (id) {
         case GestureId.rock:
-            return fill ? <FaHandRock /> : <FaRegHandRock />;
+            return fill ? (
+                <FaHandRock {...props} />
+            ) : (
+                <FaRegHandRock {...props} />
+            );
         case GestureId.paper:
-            return fill ? <FaHandPaper /> : <FaRegHandPaper />;
+            return fill ? (
+                <FaHandPaper {...props} />
+            ) : (
+                <FaRegHandPaper {...props} />
+            );
         case GestureId.scissors:
-            return fill ? <FaHandScissors /> : <FaRegHandScissors />;
+            return fill ? (
+                <FaHandScissors {...props} />
+            ) : (
+                <FaRegHandScissors {...props} />
+            );
     }
 }
