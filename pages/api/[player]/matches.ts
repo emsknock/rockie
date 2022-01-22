69a6a6d7e19aa9b36bd83f6ea7f1a48088d622a3
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 04:46:58 +0200

    Move player matches to dynamic route

---
+++ pages/api/[player]/matches.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { refreshDatabase, getPlayerMatches } from "bad-api-service/history";
+import {
+    isValidCursor,
+    PlayerMatchesPage,
+} from "bad-api-service/history/database/queries/player-matches";
+import useSWR from "swr";
+
+export const usePlayerMatches = (name: string, cursor?: string) => {
+    return useSWR<PlayerMatchesPage>(
+        cursor
+            ? `/api/${name}/matches?cursor=${cursor}`
+            : `/api/${name}/matches`
+    );
+};
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    await refreshDatabase();
+
+    const name = req.query.player as string;
+    const cursor = req.query.cursor as string | undefined;
+
+    if (cursor && !isValidCursor(cursor))
+        return res.status(400).send("Invalid cursor");
+
+    const page = await getPlayerMatches(name, cursor);
+
+    return res.json(page);
+}
