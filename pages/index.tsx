ede80399fe40e9d0f3d7c3f8eb0ef51fa438abdb
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 17:07:14 +0200

    Use text input component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from "next";
 import { useState } from "react";
-import useSWR from "swr";
 
 import { PlayerSearchResults } from "components/player-search-results";
+import { TextInput } from "components/text-input";
+import { FaSearch } from "react-icons/fa";
 
 const Home: NextPage = () => {
     const [name, setName] = useState("");
-
     return (
         <>
             <div
@@ -19,16 +19,10 @@ const Home: NextPage = () => {
                     border-b
                 `}
             >
-                <input
-                    type="text"
+                <TextInput
                     value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className={`
-                        px-4 py-2
-                        w-80
-                        rounded-full
-                        border border-gray-300
-                    `}
+                    onChange={(e) => setName(e)}
+                    icon={<FaSearch />}
                 />
             </div>
             <ol
