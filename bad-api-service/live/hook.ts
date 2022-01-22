7d25cf9b9e55dcc137893465aa132fecb846ed2a
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 07:42:25 +0200

    Create player update watcher hook

--- bad-api-service/live/hook.ts
+++ bad-api-service/live/hook.ts
@@ -1,14 +1,13 @@
-import useSWR from "swr";
-import { watcherUrl } from "utils/env";
 import useSocketState, { OngoingMatch, ResolvedMatch } from "./socket-state";
+import { useEffect, useState } from "react";
+import { watcherUrl } from "utils/env";
+import useSWR from "swr";
 
 export const useLiveState = () => {
     const socket = useSocketState();
     const fallback = useSWR<(ResolvedMatch | OngoingMatch)[]>(
         !socket.connected && watcherUrl,
-        {
-            refreshInterval: 5000,
-        }
+        { refreshInterval: 5000 }
     );
 
     // If socket is connected, we can just show the up-to-date matches,
@@ -23,4 +22,21 @@ export const useLiveState = () => {
         // In fallback mode games are cleared fallback-server-side
         clearGame: socket.clearMatchById,
     };
+};
+
+export const usePlayerUpdateWatcher = (name: string) => {
+    const [available, setAvailable] = useState(false);
+    const { matches } = useLiveState();
+
+    const playersWithNewData = new Set(
+        matches.map((g) => (g.isResolved ? [g.aPlayer, g.aPlayer] : [])).flat()
+    );
+
+    useEffect(() => {
+        if (playersWithNewData.has(name)) setAvailable(true);
+    }, Array.from(playersWithNewData));
+
+    const clearNotification = () => setAvailable(false);
+
+    return [available, clearNotification] as const;
 };
