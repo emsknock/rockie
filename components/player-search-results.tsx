f2f9dc322560511663572f7388c3bf4104b027f4
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 18:21:34 +0200

    Add message on no results

--- components/player-search-results.tsx
+++ components/player-search-results.tsx
@@ -5,22 +5,28 @@ import clsx from "clsx";
 
 export const PlayerSearchResults: FC<{ prefix: string }> = ({ prefix }) => {
     const { data } = useSWR<string[]>(`/api/find-names?name=${prefix}`);
-    return (
-        <ol className={clsx("p-2", "flex flex-wrap gap-2 justify-center")}>
-            {data?.map((name) => (
-                <li key={name} className="m-2">
-                    <Link href={`/player/${name}`}>
-                        <a
-                            className={clsx(
-                                "px-3 py-2",
-                                "font-bold text-blue-500"
-                            )}
-                        >
-                            {name}
-                        </a>
-                    </Link>
-                </li>
-            ))}
-        </ol>
-    );
+    return data ? (
+        data.length > 0 ? (
+            <ol className={clsx("p-2", "flex flex-wrap gap-2 justify-center")}>
+                {data.map((name) => (
+                    <li key={name} className="m-2">
+                        <Link href={`/player/${name}`}>
+                            <a
+                                className={clsx(
+                                    "px-3 py-2",
+                                    "font-bold text-blue-500"
+                                )}
+                            >
+                                {name}
+                            </a>
+                        </Link>
+                    </li>
+                ))}
+            </ol>
+        ) : (
+            <div className={clsx("p-2")}>
+                No players whose name begins with <q>{prefix}</q> it seems...
+            </div>
+        )
+    ) : null;
 };
