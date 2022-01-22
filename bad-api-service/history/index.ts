a9e1fa8cace7bfa2ab63b54184eb8e5944abb8e4
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 06:47:19 +0200

    Rename player-matches to history-cursor

--- bad-api-service/history/index.ts
+++ bad-api-service/history/index.ts
@@ -2,12 +2,12 @@ export { default } from "./database/connection";
 
 export { default as refreshDatabase } from "./update-db-from-api";
 
-export { default as getPlayerMatches } from "./database/queries/player-matches";
+export { default as getPlayerMatches } from "./database/queries/history-cursor";
 export { default as getLastCursor } from "./database/queries/bad-api-cursors";
 export { default as getPlayerStats } from "./database/queries/player-stats";
 
 export type {
     MatchRecord,
     PlayerMatchesPage,
-} from "./database/queries/player-matches";
+} from "./database/queries/history-cursor";
 export type { StatsRecord } from "./database/queries/player-stats";
