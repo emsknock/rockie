afd6abbf11a85be2e5e377e447b7c3d496852aed
emsknock <25252659+emsknock@users.noreply.github.com>
2022-01-22 08:28:01 +0200

    Remove unnecessary preload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,13 @@
 import type { FC } from "react";
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import { SWRConfig } from "swr";
 
-import { watcherUrl } from "utils/env";
 import { LiveLayout } from "components/live-layout";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const App: FC<AppProps> = ({ Component: Page, pageProps }) => (
     <SWRConfig value={{ fetcher }}>
-        <Head>
-            <link
-                rel="preload"
-                href={watcherUrl}
-                as="fetch"
-                crossOrigin="anonymous"
-            />
-        </Head>
         <LiveLayout>
             <Page {...pageProps} />
         </LiveLayout>
